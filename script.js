51a4b79aa0cb43f77fc10652ff7bbd08a4c444d9
umaprasath <umaprasath.lru@gmail.com>
2020-02-25 22:26:44 +1100

    search updtes
    * added search form
    * added filter on search

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 
-const pickList = document.querySelector('#pick-list');
+let pickList = document.querySelector('#pick-list');
 const form = document.querySelector('#add-picklist-form');
+const searchForm = document.querySelector('#search-picklist-form');
 
 // create element & render cafe
 function renderPickList(doc){
+  
     let li = document.createElement('li');
     let quantity = document.createElement('span');
     let partId = document.createElement('span');
@@ -18,13 +20,40 @@ function renderPickList(doc){
   pickList.appendChild(li);
 }
 
-db.collection('Picklist').where('Part ID','==','1').orderBy('Quantity').get().then((snapshot)=> {
+// db.collection('Picklist').where('Part ID','==','1').orderBy('Quantity').get().then((snapshot)=> {
+// 	console.log(snapshot.docs);
+// 	snapshot.docs.forEach(doc =>{
+// 		console.log(doc.data());
+// 		renderPickList(doc);
+// 	})
+// })
+
+db.collection('Picklist').orderBy('Quantity').get().then((snapshot)=> {
 	console.log(snapshot.docs);
 	snapshot.docs.forEach(doc =>{
 		console.log(doc.data());
 		renderPickList(doc);
 	})
 })
+
+searchForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  var child = pickList.lastElementChild;  
+  while (child) { 
+    pickList.removeChild(child); 
+      child = pickList.lastElementChild; 
+  } ;
+  db.collection('Picklist').where('Part ID','==',searchForm.partid.value).orderBy('Quantity').get().then((snapshot)=> {
+    console.log(snapshot.docs);
+    snapshot.docs.forEach(doc =>{
+      console.log(doc.data());
+      renderPickList(doc);
+    })
+  })
+  form.partid.value = '';
+  form.quantity.value = '';
+});
+
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
