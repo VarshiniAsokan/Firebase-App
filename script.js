5ab14d9a6adabac70fee3e2e5d0ac5f6c025d8e4
VarshiniAsokan <61392026+VarshiniAsokan@users.noreply.github.com>
2020-02-25 10:54:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function renderPickList(doc){
   pickList.appendChild(li);
 }
 
-db.collection('Picklist').where('Quantity','>=','50').get().then((snapshot)=> {
+db.collection('Picklist').where('Quantity','>=','50').orderBy('Quantity').get().then((snapshot)=> {
 	console.log(snapshot.docs);
 	snapshot.docs.forEach(doc =>{
 		console.log(doc.data());
